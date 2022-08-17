b2aa1a7dd54afa01d0ea988cc1187cee13ab66a0
okputu julius <juliusstan10@gmail.com>
2022-08-17 22:16:29 +0100

    project setup
    -create env vars
    -create apps
    -install dependencies
    -create custom user model,manager and tests
    -register models in the admin-create custom forms

---
+++ accounts/admin.py
@@ -0,0 +1,34 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .import models
+from .import forms
+
+# Register your models here.
+@admin.register(models.CustomUser)
+class CustomUserAdmin(UserAdmin):
+    add_form = forms.CustomUserCreationForm
+    form = forms.CustomUserChangeForm
+    model = models.CustomUser
+
+    ordering = ["email"]
+    list_display = ["email", "is_staff", "is_active","is_superuser"]
+    search_fields = ["email"]
+    
+    fieldsets = (
+        (None, {
+            "fields": (
+                'email', 'password'
+            ),
+        }),
+        ('Permissions', {
+            "fields": (
+                'is_staff', 'is_active'
+            ),
+        }),
+    )
+    add_fieldsets = (
+    (None, {
+        'classes': ('wide',),
+        'fields': ('email', 'password1', 'password2'),
+    }),
+)
